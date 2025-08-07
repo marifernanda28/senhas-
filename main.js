3b74a84c5b8dfae432c28aa6c692ddfe2a8be7c4
marifernanda28 <132498100+marifernanda28@users.noreply.github.com>
2025-08-07 10:12:58 -0300

    site

--- main.js
+++ main.js
@@ -22,3 +22,18 @@ function aumentaTamanho(){
 }
 
 console.log(botoes)
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+
