6de1d6e094c26c68b8288d63e1371cf68b6b96b5
marifernanda28 <132498100+marifernanda28@users.noreply.github.com>
2025-08-26 10:14:39 -0300

    site

--- main.js
+++ main.js
@@ -77,5 +77,5 @@ function classificaSenha(tamanhoAlfabeto){
     }
 }
 const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
